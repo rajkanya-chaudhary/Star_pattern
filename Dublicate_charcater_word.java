2bcd65e60c98ed7758f2d75ce48700770e0e66a4
rajkanya-chaudhary <dc5068786@gmail.com>
2024-07-20 23:02:43 +0530

    this is a duplicate charcater word Program using  for loop

---
+++ Dublicate_charcater_word.java
@@ -0,0 +1,50 @@
+// public class Dublicate_charcater_word {
+    //     public static void main(String[] args) {
+    //         String str = "dimpal is a good girl";
+    //         countDuplicateCharacters(str);
+    //     }
+    
+    //     public static void countDuplicateCharacters(String str) {
+    //         // Convert the string to a character array
+    //         char[] charArray = str.toCharArray();
+    //         int[] counts = new int[charArray.length];
+    
+    //         // Initialize counts array with zeros
+    //         for (int i = 0; i < counts.length; i++) {
+    //             counts[i] = 0;
+    //         }
+    
+    //         // Traverse the character array
+    //         for (int i = 0; i < charArray.length; i++) {
+    //             for (int j = i + 1; j < charArray.length; j++) {
+    //                 if (charArray[i] == charArray[j]) {
+    //                     counts[i]++;
+    //                 }
+    //             }
+    //         }
+    
+    //         // Display the characters that have a count greater than 1 (duplicate characters)
+    //         System.out.println("Duplicate characters in the given string:");
+    //         for (int i = 0; i < counts.length; i++) {
+    //             if (counts[i] > 0) {
+    //                 System.out.println(charArray[i] + ": " + (counts[i] + 1));
+    //             }
+    //         }
+    //     }
+    // }
+
+    public class Dublicate_charcater_word {
+        public static void countDuplicateCharacters(String str) {
+            String arr[] = str.split(" ");
+
+
+        }
+
+        public static void main(String[] args) {
+            String str = "dimpal is a good girl";
+
+        }
+
+    }
+    
+
